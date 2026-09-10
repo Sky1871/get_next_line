9549bd79a6fbefd643cae88573d2173ea810dcda
kseltenr <seltenreichkristof@gmail.com>
2026-09-10 03:40:19 +0200

    more helper functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,22 +6,57 @@
 /*   By: kseltenr <kseltenr@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 21:54:35 by kseltenr          #+#    #+#             */
-/*   Updated: 2026/09/10 02:35:41 by kseltenr        ###   ########.fr        */
+/*   Updated: 2026/09/10 03:39:54 by kseltenr        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+int	ft_strlen(char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char *buf, char *str)
+{
+	int		i;
+	int		n;
+	char	*ret;
+
+	i = ft_strlen(buf);
+	n = ft_strlen(str);
+	ret = malloc(sizeof(char) * (i + n + 1));
+	if (!ret)
+	{
+		free(str);
+		free(buf);
+		return (NULL);
+	}
+	i = -1;
+	n = -1;
+	while (buf[++i])
+		ret[i] = buf[i];
+	while (str[++n])
+		ret[i++] = str[n];
+	ret[i] = '\0';
+	free(buf);
+	free(str);
+	return (ret);
+}
 
 char	*delete_line(char *buf, int len)
 {
 	int		i;
 	char	*ret;
 
-	i = 0;
 	if (!buf)
 		return (NULL);
-	while (buf[i])
-		i++;
+	i = ft_strlen(buf);
 	if (len >= i)
 	{
 		free(buf);
